5eae4460be5ffe7b2d2bd2f217770d3c516b872b
Alicia Moreno Alvarez <alimoralvarez@gmail.com>
2020-09-10 17:00:13 +0200

    Creacion clases Splash: Model, ViewController y Delegate

---
+++ InfuApp/InfuApp/Features/Splash/SplashViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  SplashViewModel.swift
+//  InfuApp
+//
+//  Created by Alicia Moreno Alvarez on 10/09/2020.
+//  Copyright © 2020 Alicia Moreno Alvarez. All rights reserved.
+//
+
+import Foundation
