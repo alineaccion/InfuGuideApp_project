7a6ef808d3c79c2d0db9126c4ba70cafaf6f75ac
Alicia Moreno Alvarez <alimoralvarez@gmail.com>
2020-09-17 17:51:54 +0200

    Rename clases

--- InfuApp/InfuApp/Features/Collection List/CollectionListDelegate.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CollectionListDelegate.swift
-//  InfuApp
-//
-//  Created by Alicia Moreno Alvarez on 15/09/2020.
-//  Copyright © 2020 Alicia Moreno Alvarez. All rights reserved.
-//
-
-import Foundation
--- InfuApp/InfuApp/Splash/SplashViewDelegate.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SplashViewDelegate.swift
-//  InfuApp
-//
-//  Created by Alicia Moreno Alvarez on 10/09/2020.
-//  Copyright © 2020 Alicia Moreno Alvarez. All rights reserved.
-//
-
-import Foundation
