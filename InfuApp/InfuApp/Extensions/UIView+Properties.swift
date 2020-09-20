bc48232bc2834c07801525ef474bc02751f77b98
Alicia Moreno Alvarez <alimoralvarez@gmail.com>
2020-09-20 19:11:39 +0200

    Se añaden propiedades IBInspectables para modificar la vista sin codigo (en storyboard)

---
+++ InfuApp/InfuApp/Extensions/UIView+Properties.swift
@@ -0,0 +1,40 @@
+//
+//  UIView+Properties.swift
+//  InfuApp
+//
+//  Created by Alicia Moreno Alvarez  on 20/09/2020.
+//  Copyright © 2020 Alicia Moreno Alvarez. All rights reserved.
+//
+import UIKit
+// Para modificar vistas en storyboard sin codigo
+//https://stackoverflow.com/questions/54999229/cornerradius-and-shadow-on-a-uiview-using-ibinspectable-swift-4-2
+
+extension UIView {
+
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
