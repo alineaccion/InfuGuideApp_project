dfb887a62d05cc26f6840b04e42cae8923f1fcb7
Alicia Moreno Alvarez <alimoralvarez@gmail.com>
2020-09-22 01:42:05 +0200

    Clase Clock con recursos

---
+++ InfuApp/InfuApp/Features/4. Clock/ClockViewController.swift
@@ -0,0 +1,138 @@
+//
+//  ClockViewController.swift
+//  InfuApp
+//
+//  Created by Alicia Moreno Alvarez on 21/09/2020.
+//  Copyright © 2020 Alicia Moreno Alvarez. All rights reserved.
+//
+
+import Foundation
+import Lottie
+import RxSwift
+
+
+class ClockViewController: UIViewController {
+    
+    static var storyboardId = "ClockViewController"
+    static var storyboardName = "Clock"
+    
+    let viewModel = ClockViewModel()
+    
+    private let disposeBag = DisposeBag()
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var startStopButton: UIButton!
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var lottieContainerView: UIView!
+    private var animationView: AnimationView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureObservers()
+        viewModel.onViewLoaded()
+        configureViews()
+        configureLottieAnimation()
+    }
+    
+    func configureObservers() {
+        viewModel.needUpdateCountDown
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: {[weak self] timeInSeconds in
+                self?.uploadCountDownLabel(timeInSeconds: timeInSeconds)
+            })
+            .disposed(by: disposeBag)
+        
+        
+            viewModel.needFinishCountDown
+                .observeOn(MainScheduler.instance)
+                .subscribe(onNext: {[weak self] isFinish in
+                    self?.finishCountDown()
+                })
+                .disposed(by: disposeBag)
+    }
+    
+    func configureViews() {
+        let backButton = UIImage(named: "ic_back")
+        self.navigationController?.navigationBar.backIndicatorImage = backButton
+        self.navigationController?.navigationBar.backIndicatorTransitionMaskImage = backButton
+        self.navigationController?.navigationBar.tintColor = .font
+        
+        addLogoToNavigationBarItem()
+        resetButton.isEnabled = false
+        infoLabel.text = viewModel.infoLabel()
+    }
+    
+    func configureLottieAnimation() {
+        // 2. Start AnimationView with animation name (without extension)
+         
+         animationView = .init(name: "clock")
+         
+         animationView!.frame = lottieContainerView.bounds
+         
+         // 3. Set animation content mode
+         
+         animationView!.contentMode = .scaleAspectFit
+         
+         // 4. Set animation loop mode
+         
+         animationView!.loopMode = .loop
+         
+         // 5. Adjust animation speed
+         
+         animationView!.animationSpeed = 1.0
+         
+        lottieContainerView.addSubview(animationView!)
+    }
+    
+    private func finishCountDown() {
+        
+        animationView!.stop()
+        configureButtonStart()
+        
+        let alertController = UIAlertController(title: "¡Enhorabuena!",
+                                                message: "¡Tu infusión de \(viewModel.getInfussionName()) ya está lista!!",
+                                                preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Genial!!",
+                                                style: .default))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    private func uploadCountDownLabel(timeInSeconds: String) {
+        self.timerLabel.text = timeInSeconds
+    }
+    
+    @IBAction private  func startAction(_ sender: Any) {
+        if startStopButton.titleLabel?.text == "Iniciar" {
+            viewModel.startAction()
+            animationView!.play()
+            resetButton.isEnabled = true
+            configureButtonStop()
+        } else {
+            viewModel.stopAction()
+            animationView!.stop()
+            configureButtonStart()
+        }
+    }
+    
+    @IBAction private  func resetAction(_ sender: Any) {
+        viewModel.resetAction()
+        resetButton.isEnabled = false
+        startStopButton.setTitle("Iniciar", for: .normal)
+        animationView!.stop()
+    }
+    
+    private func configureButtonStart() {
+        startStopButton.setTitle("Iniciar", for: .normal)
+        startStopButton.backgroundColor = .buttonBackground
+        startStopButton.setTitleColor(.font, for: .normal)
+    }
+    
+    private func configureButtonStop() {
+        startStopButton.setTitle("Parar", for: .normal)
+        startStopButton.backgroundColor = .splashBackground
+        startStopButton.setTitleColor(.red, for: .normal)
+    }
+}
+
+
---
+++ InfuApp/InfuApp/Features/4. Clock/ClockViewModel.swift
@@ -0,0 +1,102 @@
+//
+//  ClockViewModel.swift
+//  InfuApp
+//
+//  Created by Alicia Moreno Alvarez on 21/09/2020.
+//  Copyright © 2020 Alicia Moreno Alvarez. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class ClockViewModel {
+    
+    var needUpdateCountDown: PublishSubject<String> = PublishSubject()
+    var needUpdateButtons: PublishSubject<Bool> = PublishSubject()
+    var needFinishCountDown: PublishSubject<Bool> = PublishSubject()
+    
+    var infussionId: Int?
+    var infussion: Infussion?
+    private var timer: Timer?
+    private var counter = 0
+    var infuAppData: InfuAppData?
+    
+    func onViewLoaded() {
+        infussion = getInfussion()
+        counter = getTimeInSeconds()
+        formatCounter()
+    }
+    
+    func getInfussion() -> Infussion? {
+        guard let infussionObtained = infuAppData?.infussions.first(where: {$0.id == infussionId}) else { return nil }
+        return  infussionObtained
+    }
+    
+    func getInfussionName() -> String {
+        guard let infussionName = infussion?.title else { return "" }
+        return infussionName
+    }
+    
+    func infoLabel() -> String  {
+        guard let time = infussion?.tags.time, let infussionName = infussion?.title else { return "" }
+        return "Preparando tú infusión de \(infussionName) - \(time) min"
+    }
+    
+    private func getTime() -> String {
+        guard let time = infussion?.tags.time else { return "" }
+        return "\(time)"
+    }
+    
+    private func getTimeInSeconds() -> Int {
+        guard let time = infussion?.tags.time else { return 0 }
+        return time * 60
+    }
+    
+    func startAction() {
+        initCountDown()
+    }
+    
+    func stopAction() {
+        stopTimer()
+    }
+    
+    func resetAction() {
+        stopTimer()
+        counter = getTimeInSeconds()
+        formatCounter()
+    }
+    
+    private func initCountDown() {
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        updateButtons(resetButtons: false)
+    }
+    
+    private func updateButtons(resetButtons: Bool) {
+        needUpdateButtons.onNext(resetButtons)
+    }
+    
+    @objc private func updateCounter() {
+        if counter > 0 {
+            counter -= 1
+            formatCounter()
+        } else {
+            stopTimer()
+            needFinishCountDown.onNext(true)
+        }
+    }
+    
+    private func formatCounter() {
+        let minutes = Int(counter) / 60 % 60
+        let seconds = Int(counter) % 60
+        needUpdateCountDown.onNext(String(format:"%02i:%02i", minutes, seconds))
+    }
+    
+    private func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+
+    private func finishCountDown() {
+        
+    }
+}
