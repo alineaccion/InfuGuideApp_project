093781de321c37748159d371f80694e0bb87bf95
Alicia Moreno Alvarez <alimoralvarez@gmail.com>
2020-09-22 01:33:57 +0200

    Extension para dar color a los iconos de navegacion

---
+++ InfuApp/InfuApp/Extensions/ColorSetting.swift
@@ -0,0 +1,19 @@
+//
+//  ColorSetting.swift
+//  InfuApp
+//
+//  Created by Alicia Moreno Alvarez on 21/09/2020.
+//  Copyright © 2020 Alicia Moreno Alvarez. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static var primary: UIColor { UIColor(named: "Primary")! }
+    static var primaryLight: UIColor { UIColor(named: "PrimaryLight")! }
+    static var primaryDark: UIColor { UIColor(named: "PrimaryDark")! }
+    static var buttonBackground: UIColor { UIColor(named: "ButtonBackground")! }
+    static var font: UIColor { UIColor(named: "Font")! }
+    static var line: UIColor { UIColor(named: "Line")! }
+    static var splashBackground: UIColor { UIColor(named: "SplashBackground")! }
+}
